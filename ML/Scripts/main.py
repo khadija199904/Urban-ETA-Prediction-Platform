b5a3e7ae2e494e698ea294cc0d766c6c6d929300
Khadija Elabbioui <khadijaelabbioui1999@email.com>
2026-01-15 18:02:39 +0100

    SL-19 Développement du script d'Entraînement

--- ML/Scripts/main.py
+++ ML/Scripts/main.py
@@ -1,9 +1,11 @@
 import pandas as pd
-from sqlalchemy import create_engine
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.pipeline import Pipeline
 import joblib 
 from database import engine
 
@@ -12,10 +14,10 @@ query = "SELECT * FROM silver_taxi"
 print("Chargement des données depuis PostgreSQL...")
 
 df = pd.read_sql(query, engine)
-
+print(df.head(5))
 # Préparation des données (Preprocessing)
 target = "trip_duree"
-features = ["trip_distance", "fare_amount", "tip_amount", "total_amount","cbd_congestion_fee", "pickup_hour", "day_of_week", "month"]
+features = ["trip_distance","fare_amount","tip_amount","total_amount","cbd_congestion_fee","pickup_hour","day_of_week","month","RatecodeID","payment_type"]
 
 X = df[features]
 y = df[target]
@@ -24,31 +26,45 @@ y = df[target]
 
 # 4. Split Train/Test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+models = [
+    ("LinearRegression", LinearRegression()),
+    ("RandomForest", RandomForestRegressor(random_state=42)),
+    ("GBT", GradientBoostingRegressor(random_state=42))
+]
 
-# Normalisation 
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+# Boucle d'entraînement avec Pipeline
+best_rmse = float('inf')
+best_pipe = None
+best_model_name = None
 
-#  Entraînement du modèle (Gradient Boosting comme dans votre exemple Spark)
-print("Entraînement du modèle Scikit-Learn...")
-model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=5, random_state=42)
-model.fit(X_train_scaled, y_train)
+for name, model in models:
+    print(f"\n--- Entraînement de : {name} ---")
+    
+    
+   
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('regressor', model)
+    ])
 
-# 7. Évaluation
-predictions = model.predict(X_test_scaled)
-rmse = mean_squared_error(y_test, predictions, squared=False)
-r2 = r2_score(y_test, predictions)
+    # Entraînement
+    pipeline.fit(X_train, y_train)
+    
+    # Prédictions et évaluation
+    y_pred = pipeline.predict(X_test)
+    r2 = r2_score(y_test, y_pred)
+    rmse = mean_squared_error(y_test, y_pred)
+    
+    print(f"R²: {r2:.2f} | RMSE: {rmse:.2f}")
+    if rmse < best_rmse:
+        best_rmse = rmse
+        best_pipe = pipeline
+        best_model_name = name
+     # Sauvegarde du modèle
+filename = f"ML/models_pkl/{best_model_name}_Sklearn.pkl"
+joblib.dump(best_pipe, filename)
 
-print(f"\nRésultats du modèle Scikit-Learn :")
-print(f"R²: {r2:.4f}")
-print(f"RMSE: {rmse:.4f}")
+print(f"\nMeilleur modèle sauvegardé dans  {filename} avec RMSE: {best_rmse:.2f}")
 
-# # 8. Sauvegarde du modèle et du scaler
-# import os
-# os.makedirs("models_sklearn", exist_ok=True)
 
-# joblib.dump(model, 'models_sklearn/gbt_model.pkl')
-# joblib.dump(scaler, 'models_sklearn/scaler.pkl')
 
-# print("Modèle sauvegardé dans 'models_sklearn/'")
