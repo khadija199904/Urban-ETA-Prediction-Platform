8b77aa8dce93bae599bdf14e80dcf495c97d17df
Khadija Elabbioui <khadijaelabbioui1999@email.com>
2026-01-18 05:28:32 +0100

    Changes

--- api/core/config.py
+++ api/core/config.py
@@ -6,11 +6,12 @@ import os
 load_dotenv()
 
 # Fetch variables
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
+USER = os.getenv("user", "postgres")
+PASSWORD = os.getenv("password", "password")
+HOST = os.getenv("host", "localhost")
+PORT = os.getenv("port", "5432")
+DBNAME = os.getenv("dbname", "test_db")
+
 
 # Construct the SQLAlchemy connection string
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
